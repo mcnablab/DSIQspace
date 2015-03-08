ad7c58ecdd0112b5b0adb345bb1564c88987e850
Qiyuan Tian <fdqiyuantian@gmail.com>
2015-03-08 10:48:18 -0700

    Codes in dsitool to swap the data such that the vis is good. Rewrote script 2 and 5

--- data/OLD/modifybvec.m
+++
@@ -1,8 +0,0 @@
-clear, clc, close all
-
-bvecs = importdata('DSI11_exvivo_bvecs.txt');
-tmp = bvecs(:, 2);
-bvecs(:, 2) = bvecs(:, 3);
-bvecs(:, 3) = -tmp;
-dlmwrite('DSI11_exvivo_bvecslala.txt', bvecs, 'delimiter', ' ');
-
